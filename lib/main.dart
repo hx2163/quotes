f66882bc0e4f3956b6588d4e0a271a345816eeba
hx2163 <hx2163@nyu.edu>
2023-09-30 13:34:55 -0400

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -12,13 +13,22 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Osca Wilde', text:'Be yourself; everyone else is already taken'),
+    Quote(author: 'Osca Wilde', text:'I have nothing to declare except my genius'),
+    Quote(author: 'Osca Wilde', text:'The truth is rarely pure and never simple'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
+        title: Text('Awesome Quotes'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
