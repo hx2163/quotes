6ce406645b8786c79d2ffedc7857f09c9aa85b3c
hx2163 <hx2163@nyu.edu>
2023-09-30 12:32:53 -0400

    update text

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
